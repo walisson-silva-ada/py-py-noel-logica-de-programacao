7779b6f4bb98caa11665ebd0d15eb4f1c6558e89
ACarolinaS <89361247+ACarolinaS@users.noreply.github.com>
2022-01-26 11:09:14 -0300

    Update SuperDigito.py
    Correções sugeridas:
    comentários e contador retirados

--- SuperDigito.py
+++ SuperDigito.py
@@ -1,20 +1,13 @@
 def super_digito(str_numero):
     lista = []
-    contador = 0
     lista.append(str_numero)
-    
     sup = list(map(int, str_numero))
-    
     soma = sum(sup)
-    
-#     print(soma)
     
     while soma > 9:
         novo_num = str(soma)
         lista.append(novo_num)
         soma = super_digito(novo_num)
-        
-#     print(lista)
     return(soma)
 
 print('\::: Bem vindo ao cálculo de super dígito :::\n')
@@ -22,5 +15,4 @@ n = int(input('Digite um número:'))
 k = int(input('Digite um número:'))
 str_numero = str(k*n)
 
-resultado = super_digito(str_numero)
-print('O super dígito do produto dos números digitados é', resultado)
+print('O super dígito do produto dos números digitados é', super_digito(str_numero))
